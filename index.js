bd16104e7edc6738cf172e604d523d2ffa3bf66e
James Kent <jkent@blueengine.com.au>
2023-04-26 11:51:24 +1000

    Write Certificate to Disk

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
+const fs = require('fs/promises');
 
 const sfdx = require('sfdx-node');
 
@@ -13,19 +15,23 @@ async function run() {
     if (!clientId) throw new Error('Missing Connected App Client ID');
     if (!username) throw new Error('Missing Salesforce Username');
 
-    console.log(`Hello ${serverKey === null} ${serverKey === undefined} ${!serverKey}!`);
-  
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
   
+    const serverKeyFilepath = path.join(process.env['RUNNER_TEMP'], 'server.key');
+    console.log('serverKeyFilepath', serverKeyFilepath);
+    const fsWriteResult = await fs.writeFile(serverKeyFilepath, serverKey);
+    console.log('fsWriteResult', fsWriteResult);
+
     /*
     // sfdx force auth jwt grant --client-id $SFDX_CLIENT_ID --jwt-key-file ./config/server.key --username $SFDX_USERNAME --alias $SFDX_ALIAS
-    sfdx.force.auth.jwt({
+    const result = await sfdx.force.auth.jwt({
       "client-id": clientId,
       "jwt-key-file": 
     })
     */
+
   }
   catch (error) {
     core.setFailed(error.message);
