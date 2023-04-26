b181fe0f86cb878447360d1086d7d8e99456e4a2
James Kent <jkent@blueengine.com.au>
2023-04-26 10:50:21 +1000

    Initial

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
+const sfdx = require('sfdx-node');
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
+    const serverKey = core.getInput('serverKey');
+    const clientId = core.getInput('clientId');
+    const username = core.getInput('username');
+  
+    console.log(`Hello ${serverKey}!`);
+  
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  
+    /*
+    // sfdx force auth jwt grant --client-id $SFDX_CLIENT_ID --jwt-key-file ./config/server.key --username $SFDX_USERNAME --alias $SFDX_ALIAS
+    sfdx.force.auth.jwt({
+      "client-id": clientId,
+      "jwt-key-file": 
+    })
+    */
+  }
+  catch (error) {
     core.setFailed(error.message);
   }
 }
